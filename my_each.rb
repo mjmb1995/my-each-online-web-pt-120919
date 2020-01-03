9aa86e79ef5e3aafadecfa0e071cbd618906c6e8
Mark Bihag <markbihag330@gmail.com>
2020-01-03 03:28:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+  count = 0
+  while collection.length > count
+    yield(collection[count])
+    count += 1
+  end
 end
